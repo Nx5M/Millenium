8b09d6b1311b46e67c2fafc514d15e794d3937a1
Nx5M <77025467+Nx5M@users.noreply.github.com>
2026-04-03 11:59:49 +0700

    Update nigga.lua

--- nigga.lua
+++ nigga.lua
@@ -781,7 +781,7 @@
                     Name = "\0";
                     TextColor3 = themes.preset.accent;
                     BorderColor3 = rgb(0, 0, 0);
-                    Text = '<font color="rgb(72, 72, 73)">' .. cfg.expire .. ', </font>' .. cfg.name .. " " .. cfg.suffix;
+                    Text = "";
                     Size = dim2(1, 0, 0, 0);
                     Position = dim2(0, -10, 0.5, -1);
                     AnchorPoint = vec2(0, 0.5);
